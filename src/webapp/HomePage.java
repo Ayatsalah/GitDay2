42437edb0fefa032876ba18f537430b377bd15e1
Ayat <eng.hard@yahoo.com>
2024-02-01 13:43:47 +0000

    Home page created

---
+++ src/webapp/HomePage.java
@@ -0,0 +1,12 @@
+package webapp;
+
+public class HomePage {
+
+    //Setting up homepage
+
+    //nav menu
+    public static void main(String[] args) {
+        System.out.println("Ayat");
+
+    }
+}
